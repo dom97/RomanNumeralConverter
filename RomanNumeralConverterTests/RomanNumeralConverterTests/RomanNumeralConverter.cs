5b7ea3422b10eaafecd15bb1a51e5406de65635f
Dom Edwards <dedwards@qualtrax.com>
2016-04-01 16:30:29 -0400

    Initial Commit

---
+++ RomanNumeralConverterTests/RomanNumeralConverterTests/RomanNumeralConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumeralConverterTests
+{
+    static class RomanNumeralConverter
+    {
+        public static Int32 Convert(String numeral)
+        {
+            var valueOfNumeral = 0;
+
+            for (var i = 0; i < numeral.Length; i++)
+                valueOfNumeral += ValueOfBaseNumeral("" + (numeral.ElementAt(i)));
+
+            return valueOfNumeral;
+        }
+
+
+        private static Int32 ValueOfBaseNumeral(String numeral)
+        {
+            if (numeral.Equals("I"))
+                return 1;
+
+            if (numeral.Equals("V"))
+                return 5;
+
+            if (numeral.Equals("X"))
+                return 10;
+
+            if (numeral.Equals("L"))
+                return 50;
+
+            if (numeral.Equals("C"))
+                return 100;
+
+            if (numeral.Equals("D"))
+                return 500;
+
+            if (numeral.Equals("M"))
+                return 1000;
+
+            return 0;
+        }
+
+    }
+}
---
+++ RomanNumeralConverterTests/RomanNumeralConverterTests/RomanNumeralTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanNumeralConverterTests
+{
+    [TestClass]
+    public class RomanNumeralTests
+    {
+
+        [TestMethod]
+        public void ConvertI()
+        {
+            Assert.AreEqual(1, RomanNumeralConverter.Convert("I"));
+        }
+
+        [TestMethod]
+        public void ConvertV()
+        {
+            Assert.AreEqual(5, RomanNumeralConverter.Convert("V"));
+        }
+
+        [TestMethod]
+        public void ConvertX()
+        {
+            Assert.AreEqual(10, RomanNumeralConverter.Convert("X"));
+        }
+
+        [TestMethod]
+        public void ConvertL()
+        {
+            Assert.AreEqual(50, RomanNumeralConverter.Convert("L"));
+        }
+
+        public void ConvertC()
+        {
+            Assert.AreEqual(100, RomanNumeralConverter.Convert("C"));
+        }
+
+        [TestMethod]
+        public void ConvertD()
+        {
+            Assert.AreEqual(500, RomanNumeralConverter.Convert("D"));
+        }
+
+        [TestMethod]
+        public void ConvertM()
+        {
+            Assert.AreEqual(1000, RomanNumeralConverter.Convert("M"));
+        }
+
+        [TestMethod]
+        public void ConvertXI()
+        {
+            Assert.AreEqual(11, RomanNumeralConverter.Convert("XI"));
+        }
+
+    }
+}
